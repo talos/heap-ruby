b67167cd4526e4fb9489c5a5c6a7653fcab42602
talos <jkrauss@heapanalytics.com>
2019-07-31 17:39:40 -0700

    linting and fix typo

--- lib/heap/middleware.rb
+++ lib/heap/middleware.rb
@@ -5,10 +5,10 @@ class HeapAPI::Middleware
 
   def call(env)
     heap_context = {}
-    env["HTTP_COOKIE"].split("; ").each do |cookie|
-      if cookie.starts_with? "_hp2_"
-        key, value = cookie.split "="
-        cookie_type, env_id = key.split "."
+    env['HTTP_COOKIE'].split('; ').each do |cookie|
+      if cookie.starts_with? '_hp2_'
+        key, value = cookie.split '='
+        cookie_type, env_id = key.split '.'
         if not heap_context[env_id]
           heap_context[env_id] = {}
         end
@@ -19,7 +19,7 @@ class HeapAPI::Middleware
     # :KLUDGE: assuming 1:1 relation between thread and request lifecycle
     Thread.current[:heap_context] = heap_context
 
-    HeapAPI::HeapMiddleware.track "http_request", "path" => env["REQUEST_URI"]
+    HeapAPI::Middleware.track 'http_request', 'path' => env['REQUEST_URI']
 
     @status, @headers, @response = @app.call(env)
 
@@ -29,15 +29,15 @@ class HeapAPI::Middleware
   # TODO this public-facing function should not live in the middleware
   def self.track(type, custom_props)
     Thread.current[:heap_context].each do |env_id, cookies|
-      id_cookie = cookies["_hp2_id"]
+      id_cookie = cookies['_hp2_id']
       if not id_cookie
         next
       end
-      user_id = id_cookie["userId"]
-      session_id = id_cookie["sessionId"]
-      pageview_id = id_cookie["pageviewId"]
-      ses_props = cookies["_hp2_ses_props"]
-      event_props = cookies["_hp2_props"] || {}
+      user_id = id_cookie['userId']
+      session_id = id_cookie['sessionId']
+      pageview_id = id_cookie['pageviewId']
+      ses_props = cookies['_hp2_ses_props']
+      event_props = cookies['_hp2_props'] || {}
 
       properties = event_props.merge custom_props
 
